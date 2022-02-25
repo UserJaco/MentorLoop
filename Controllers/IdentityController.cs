c94fe9fdf28c214d5d824965cd320871b4df2c0e
UserJaco <96179881+UserJaco@users.noreply.github.com>
2022-02-25 04:17:46 -0800

    Add files via upload

---
+++ Controllers/IdentityController.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyAPI.Controllers
+{
+    public class IdentityController : ApiController
+    {
+        // GET: api/Identity
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Identity/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Identity
+        public bool Post([FromBody] JObject value)
+        {
+            if(value["username"].ToString().ToLower() == "jaco" && value["password"].ToString() == "password")
+            {
+                return true;
+            }
+            else if (value["username"].ToString().ToLower() == "claudine" && value["password"].ToString() == "secret")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // PUT: api/Identity/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Identity/5
+        public void Delete(int id)
+        {
+        }
+
+        [HttpPost]
+        [Route("api/Identity/LogIn")]
+        [ActionName("LogIn")]
+        [Description("Log In the user")]
+
+        public bool LogIn(string userName)
+        {
+            // Listing listing = new Listing(name, description, price);
+            // 
+            // 
+            // return listingProvider.CreateListing(listing);
+            return true;
+        }
+    }
+}
---
+++ Controllers/ListingsController.cs
@@ -0,0 +1,36 @@
+﻿using MyAPI.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace MyAPI.Controllers
+{
+    public class ListingsController : ApiController
+    {
+        // GET: api/listings
+        public IEnumerable<Listing> Get()
+        {
+            var listings = new List<Listing>();
+            Listing listing = new Listing
+            {
+                Name = "blank",
+                Description = "this string",
+                Id = 1,
+                Views = 1,
+                Price = 1000
+            };
+            listings.Add(listing);
+
+            var otherListing = new Listing
+            {
+                Name = "pietie",
+                Description = "Number that is a string",
+                Id = 2,
+                Views = 11,
+                Price = 100000
+            };
+            listings.Add(otherListing);
+
+            return listings;
+        }
+    }
+}
